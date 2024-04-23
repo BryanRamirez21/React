1fe848e30a694231d485e05a326e758f909f684d
unknown <bryan.ramirez9289@alumnos.udg.mx>
2024-04-23 09:14:40 -0600

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 //import AppRouter from './BOOTCAMP/zustand_recipies/routes'
-import State_eg1 from './OPBC/hooks/State_eg1'
-import ComponenteContexto from './OPBC/hooks/ContextHook_eg2'
-import Props_eg3 from './OPBC/hooks/Props_eg3'
-import FormHook from './BOOTCAMP/usehookform/components/Form'
+import State_eg1 from           './OPBC/hooks/State_eg1'
+import ComponenteContexto from  './OPBC/hooks/ContextHook_eg2'
+import Props_eg3 from           './OPBC/hooks/Props_eg3'
+
+import FormHook from    './BOOTCAMP/usehookform/components/Form'
 import ContactForm from './BOOTCAMP/usehookform/components/ContactForm'
+
 import AppRouter from './BC_Project/routes'
 
 function App() {
   return (
       <div>
-        <AppRouter />
+        <Props_eg3 name={"bryan"} age={22}/>
         
       </div>
   ) 
