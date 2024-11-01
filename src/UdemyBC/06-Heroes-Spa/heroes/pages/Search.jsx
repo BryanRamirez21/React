70a4734f9c327c64a62665577f8507a74422ced0
unknown <bryan.ramirez9289@alumnos.udg.mx>
2024-11-01 15:15:54 -0600

    search updated

--- src/UdemyBC/06-Heroes-Spa/heroes/pages/Search.jsx
+++ src/UdemyBC/06-Heroes-Spa/heroes/pages/Search.jsx
@@ -3,25 +3,47 @@ import { HeroCard } from '../components'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm';
 import queryString from 'query-string';
+import { getHeroesByName } from '../helpers';
 
 export const Search = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
 
-    const {q = ""} = queryString.parse(location.search)
+    const {q = ""} = queryString.parse(location.search);
+    const heroes = getHeroesByName(q)
 
     const {searchText, onInputChange} = useForm({
-        searchText: ""
+        searchText: q
     });
 
     const onSearchSubmit = (e) => {
         e.preventDefault();
-        if(searchText.trim().length <= 1){
-            return;
-        }
+        //if(searchText.trim().length <= 1) return;
 
         navigate(`?q=${searchText.toLowerCase().trim()}`)
+    }
+
+    const ResultColComponent = () => {
+        if(q.length <= 0){
+            return (
+                <div className='alert alert-primary'>
+                    Search a hero
+                </div>
+            );
+        }else if(heroes.length === 0 && q.length > 0){
+            return (
+                <div className='alert alert-danger'>
+                    No "{q}" found
+                </div>
+            );
+        }else if(heroes.length > 0){
+            return (
+                heroes.map(hero => (
+                    <HeroCard key={hero.id} hero={hero} />
+                ))
+            )
+        }
     }
 
     return (
@@ -48,14 +70,7 @@ export const Search = () => {
                 <div className="col-7">
                     <h4>Resutls</h4>
                     <hr />
-                    <div className='alert alert-primary'>
-                        Search a hero
-                    </div>
-                    <div className='alert alert-danger'>
-                        No "{q}" found
-                    </div>
-
-                    
+                    <ResultColComponent />
                 </div>
             </div>
             
