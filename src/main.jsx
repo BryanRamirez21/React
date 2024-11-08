76442b92a3c2feb34d42f5a9e00b9939e7cbf5f9
unknown <bryan.ramirez9289@alumnos.udg.mx>
2024-11-08 15:06:52 -0600

    some

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { HeroesApp } from './UdemyBC/06-Heroes-Spa/HeroesApp.jsx';
 import { MemoHook } from './UdemyBC/05-Hook-App/06-memos/MemoHook.jsx';
+import { Forma } from './UdemyBC/05-Hook-App/06-memos/memo2/Forma.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
     <BrowserRouter>
-      <MemoHook />
+      <Forma />
     </BrowserRouter>
     
   //</React.StrictMode>,
