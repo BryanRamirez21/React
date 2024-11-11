a633b65de625d17e1eeb7b9c9e219b007005ca29
unknown <bryan.ramirez9289@alumnos.udg.mx>
2024-11-11 16:11:08 -0600

    useMemo learned

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { HeroesApp } from './UdemyBC/06-Heroes-Spa/HeroesApp.jsx';
 import { MemoHook } from './UdemyBC/05-Hook-App/06-memos/MemoHook.jsx';
 import { Forma } from './UdemyBC/05-Hook-App/06-memos/memo2/Forma.jsx';
+import { CallBackHook } from './UdemyBC/05-Hook-App/06-memos/CallBackHook.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
     <BrowserRouter>
-      <Forma />
+      <CallBackHook />
     </BrowserRouter>
     
   //</React.StrictMode>,
