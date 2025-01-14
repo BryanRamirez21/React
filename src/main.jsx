89d06bbc2c5fa93b672cebba1eaa5a185cc59726
unknown <bryan.ramirez9289@alumnos.udg.mx>
2025-01-14 14:38:49 -0600

    redux toolkit learned

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './UdemyBC/08-toolkit-redux/store/Store.js'
 import { Provider } from 'react-redux'
-import { Toolkit } from './UdemyBC/08-toolkit-redux/Toolkit.jsx'
-import { PokemonApp } from './UdemyBC/08-toolkit-redux/PokemonApp.jsx'
-import { ToDoApp } from './UdemyBC/08-toolkit-redux/ToDoApp.jsx'
+import { AppYT } from './UdemyBC/08-toolkit-redux/yt/AppYT.jsx'
+import { store } from './UdemyBC/08-toolkit-redux/yt/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
     //<BrowserRouter>
       <Provider store={store}>
-        <ToDoApp />
+        <AppYT />
       </Provider>
     //</BrowserRouter>
     
