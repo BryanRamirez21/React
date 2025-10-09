4c87818bac7ccfeff19ead6b5af69384e8485c9e
Bryan Uriel Ramirez Espinosa <bryan.ramirez9289@alumnos.udg.mx>
2025-10-09 00:10:01 -0600

    [feat]: todo app functions started

--- TS/hooks-app/src/05-useReducer/TasksApp.tsx
+++ TS/hooks-app/src/05-useReducer/TasksApp.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { todo } from 'node:test';
 
 interface Todo {
   id: number;
@@ -18,23 +19,41 @@ export const TasksApp = () => {
   const [inputValue, setInputValue] = useState('');
 
   const addTodo = () => {
-    console.log('Agregar tarea', inputValue);
+    if(inputValue.length === 0) return;
 
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputValue.trim(),
+      completed: false
+    };
+
+    setTodos([...todos, newTodo]);
+    // this line is exactly the same as the normal spread, but we can use "prev" if we dont have acces to the state var
+    // setTodos((prev) => [...prev, newTodo])
+
+    setInputValue('');
   };
 
   const toggleTodo = (id: number) => {
-    console.log('Cambiar de true a false', id);
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, completed: !todo.completed}
+      }
+      return todo;
+    });
 
+    setTodos(updatedTodos);
   };
 
   const deleteTodo = (id: number) => {
-    console.log('Eliminar tarea', id);
-
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
   };
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
-    console.log('Presiono enter');
-
+    if(e.key === 'enter'){
+      addTodo();
+    }
   };
 
   const completedCount = todos.filter((todo) => todo.completed).length;
